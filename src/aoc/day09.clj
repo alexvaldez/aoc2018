f4ecf1e67d113ea3537105ba7c053ca0bd24a32b
Alex Valdez <179569+alexvaldez@users.noreply.github.com>
2019-09-06 22:16:50 -0500

    performance improvements using mutable arrays

--- src/aoc/day09.clj
+++ src/aoc/day09.clj
@@ -10,22 +10,57 @@
 (defn delete [arr i]
   (into [] (concat (subvec arr 0 i) (subvec arr (inc i)))))
 
-; TODO rewrite to use Java arrays for better performance?
 (defn day09-1 [[nplayers nmarbles]]
-  (let [[arr curr scores]
-    (reduce (fn [[arr curr scores] n]
+  (def arr (java.util.ArrayList.))
+  (.add arr 0 0)
+  (let [[curr scores]
+    (reduce (fn [[curr scores] n]
+              ;(println (.toString arr))
               (if (= 0 (mod n 23))
                 (let [marble (mod (- curr 7) (count arr))
-                      score (+ n (arr marble))]
-                  [(delete arr marble)
-                   (mod marble (dec (count arr)))
+                      score (+ n (.get arr marble))]
+                  (if (= 0 (mod n 1000))
+                    (do
+                      (printf "n %d marble %d score %d" n (.get arr marble) score)
+                      (println)))
+                  (.remove #^java.util.ArrayList arr (int marble))
+                  [(mod marble (dec (.size arr)))
                    (update-in scores [(mod n nplayers)] (fnil #(+ score %) 0))])
-                (let [nxt (inc (mod (inc curr) (count arr)))]
-                  [(add arr nxt n)
-                   nxt
+                (let [nxt (inc (mod (inc curr) (.size arr)))]
+                  (.add #^java.util.ArrayList arr (int nxt) n)
+                  [nxt
                    scores])))
-            [[0] 0 {}] 
-            ;(drop 1 (range))))
+            [0 {}] 
             (range 1 (inc nmarbles)))]
     (apply max (vals scores))))
 
+(defn day09-2 [[nplayers nmarbles]]
+  (def before (int-array (inc nmarbles)))
+  (def after (int-array (inc nmarbles)))
+  (let [[curr scores]
+    (reduce (fn [[curr scores] n]
+              ;(println (.toString arr))
+              (if (= 0 (mod n 23))
+                (let [marble (nth (iterate (partial aget before) curr) 7)
+                      nxt (aget after marble)
+                      prv (aget before marble)
+                      score (+ marble n)]
+                  (aset after prv nxt)
+                  (aset before nxt prv)
+                  (if (= 0 (mod n 1000))
+                    (do
+                      (printf "n %d marble %d score %d" n marble score)
+                      (println)))
+                  [nxt
+                   (update-in scores [(mod n nplayers)] (fnil #(+ score %) 0))])
+                (let [nxt (nth (iterate (partial aget after) curr) 2)
+                      prv (aget before nxt)]
+                  (aset after n nxt)
+                  (aset before n prv)
+                  (aset after prv n)
+                  (aset before nxt n)
+                  [n
+                   scores])))
+            [0 {}] 
+            (range 1 (inc nmarbles)))]
+    (apply max (vals scores))))
